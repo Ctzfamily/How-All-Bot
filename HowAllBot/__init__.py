9796457dd7ae66c4bc21c55abd3205e98af8e6d9
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-03-05 15:26:59 +0530

    Create __init__.py

---
+++ HowAllBot/__init__.py
@@ -0,0 +1,14 @@
+import os
+import logging
+import time
+from pyrogram import Client, filters, errors
+StartTime = time.time()
+logging.basicConfig(level=logging.INFO)
+
+LOGGER = logging.getLogger(__name__)
+
+API_ID = os.environ.get('API_ID', None)
+API_HASH = os.environ.get('API_HASH', None)
+BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+
+pbot = Client("HowAllBot", api_id = API_ID, api_hash = API_HASH, bot_token = BOT_TOKEN)
